f331d2754d93c6e54c5809d970b900b98d5fe25c
Vlad <netdesopgame@gmail.com>
2024-05-01 12:04:43 +0300

    anotation

--- block-systems-hm.php
+++ block-systems-hm.php
@@ -1,13 +1,28 @@
 <?php
 /**
- * Plugin Name: Block Systems HM
- * Plugin URI: https://example.com/
- * Description: A plugin to manage blocks in WordPress.
- * Version: 1.0
- * Author: Ваше имя
- * Author URI: https://example.com/
- */
-
+* Plugin Name: Manage blocks in WordPress.
+* Plugin URI: http://housemagik.com/plugins/block-systems-hm
+* Description: A plugin to manage blocks in WordPress.
+* Version: 1.1
+* Author: V. Kirillov HousemagiK
+* Author URI: hhttp://housemagik.com/author
+ *
+* Copyright 2024  V. Kirillov  (email: netdesopgame@gmail.com)
+ *
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+ *
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+ *
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 // If this file is called directly, abort.
 if (!defined('WPINC')) {
     die;
